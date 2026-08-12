da0b9e78bc85e2af3349a369f633280c22625626
Marylou Groeneveld <marylougroeneveld@gmail.com>
2026-08-12 20:03:18 +0200

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 
 
 function App() {
-  return <h1>Hi this will be my cool portfolio :)</h1>
+  return (
+    <div className="browser">
+      <header className="browser-bar">
+
+        <h1>margreen.dev</h1>
+
+        <div className="search-lang">
+
+          <div className="arrow-buttons">
+            <div className="arrow">←</div>
+            <div className="arrow">→</div>
+          </div>
+
+          <div className="search-bar">
+            <p>Welcome to my portfolio!</p>
+          </div>
+
+          <div className="language">
+            <img src="language.svg" alt="language" />
+            <p>NL</p> {/* will be button ig */}
+          </div>
+        </div>
+      </header>
+
+      <div className="browser-content">
+        <h1>welcome</h1>
+        <p>my cool portfolio</p>
+      </div>
+
+    </div>
+  )
 }
 
 export default App
