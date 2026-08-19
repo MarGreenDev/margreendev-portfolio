51ff5876342b757ab21996a3b4627b1022e5a255
Marylou Groeneveld <marylougroeneveld@gmail.com>
2026-08-19 21:18:33 +0200

    add quicklinks to sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -7,11 +7,23 @@ export default function Sidebar() {
 
     return (
         <aside className="info-section">
-            <img src={profilePhoto} alt="profile photo" />
+            <img src={profilePhoto} alt="profile photo" className="profile-photo" />
 
             <div className="sidebar-title">
                 <h2>{t("sidebar.title")}</h2>
                 <p>{t("sidebar.description")}</p>
+            </div>
+
+            <div className="sidebar-links">
+                <a className="link" href="https://github.com/MarGreenDev">
+                    <img src="github.svg" alt="github link" />
+                </a>
+                <a className="link" href="https://www.linkedin.com/in/marylou-groeneveld-861111217/">
+                    <img src="linkedin.svg" alt="linkedin link" />
+                </a>
+                <a className="link" href="mailto:marylougroeneveld@gmail.com">
+                    <img src="mail.svg" alt="send e-mail" />
+                </a>
             </div>
 
         </aside>
