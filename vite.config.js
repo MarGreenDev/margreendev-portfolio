9c82ebc015525e3fb806b4eeec473d41109f7c6d
Marylou Groeneveld <marylougroeneveld@gmail.com>
2026-08-25 11:14:42 +0200

    setup github actions

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/margreendev-portfolio',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
