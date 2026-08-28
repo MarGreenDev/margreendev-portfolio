bb4396301a9690fc7ea7d2cda185f074a3e8afca
Marylou Groeneveld <marylougroeneveld@gmail.com>
2026-08-28 16:55:27 +0200

    add tictactoe iframe to project page

--- src/pages/projects/tictactoe.jsx
+++ src/pages/projects/tictactoe.jsx
@@ -4,6 +4,12 @@ export default function Tictactoe() {
 
 
     return (
-        <h2>tictactoe</h2>
+        <>
+            <h2>tictactoe</h2>
+
+            <div className="container">
+                <iframe src="/tictactoe-school/index.html"></iframe>
+            </div>
+        </>
     )
 }
