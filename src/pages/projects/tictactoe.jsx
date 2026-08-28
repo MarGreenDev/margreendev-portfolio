fc1c0fb953bb4a8fa1bf79bf789ccabe90697e27
Marylou Groeneveld <marylougroeneveld@gmail.com>
2026-08-28 22:24:04 +0200

    create tictactoe preview gif for mobile devices, make page responsive

--- src/pages/projects/tictactoe.jsx
+++ src/pages/projects/tictactoe.jsx
@@ -7,16 +7,14 @@ export default function Tictactoe() {
         <>
             <h2>tictactoe</h2>
 
-            <div className="container">
+            <div className="desktop-tictactoe">
                 <iframe src="/tictactoe-school/index.html"></iframe>
             </div>
 
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia consequatur amet vel autem doloribus adipisci soluta corporis, voluptatum nesciunt error fugit harum, dignissimos sit dolore ipsam cum aspernatur fuga qui.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla fuga delectus repellendus impedit soluta expedita, mollitia modi ducimus nobis, tempora veniam dolor, quo quia quaerat perferendis non pariatur recusandae esse.
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita accusamus tempora tempore neque pariatur quisquam minima dolore necessitatibus illo quam, quod adipisci perspiciatis quasi eaque minus vel nobis perferendis ratione?
-                </p>
-            </div>
+            <figure className="mobile-tictactoe">
+                <img src="/tictactoe.gif" alt="Tic-Tac-Toe Gif preview" />
+                <figcaption>Try it out on desktop!</figcaption>
+            </figure>
 
         </>
     )
